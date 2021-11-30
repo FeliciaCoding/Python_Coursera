b594fb629b79da2d550b6215b35eff252b34b582
FeliciaCoding <peiwenliao0703@gmail.com>
2021-11-30 15:43:10 +0100

    Create Fundamental_Expression.py

---
+++ Fundamental_Expression.py
@@ -0,0 +1,16 @@
+
+#1 Using input() to plause and read data for user
+nam = input ('Who are you?')
+print('Welcome', nam)
+x = int(98.6)
+print(x)
+
+    # calculating payment with 35 hours * 2.75per hours
+        # asking for data
+hrs=input('Enter Hours')
+rate=input('Enter the rate per hour')
+        # Formula
+            # f_hrs = float(hrs)
+            # f_rate = float(rate)
+            # pay = f_hrs * f_rate
+print('pay:', float(hrs)*float(rate))
