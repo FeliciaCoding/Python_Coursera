dbdaf2b59d95838e21e08e0b0bcf48e07ebf6d83
FeliciaCoding <peiwenliao0703@gmail.com>
2021-12-28 19:46:32 +0100

    revised

---
+++ Assignment8.5.py
@@ -0,0 +1,27 @@
+#Open the file mbox-short.txt and read it line by line.
+#find a line that starts with 'From ' like the following line:
+#       From stephen.marquard@uct.ac.za Sat Jan  5 09:14:16 2008
+# parse the From line using split()
+#print out the second word in the line
+#(i.e. the entire address of the person who sent the message).
+# print out a count at the end:
+#       There were 27 lines in the file with From as the first word
+#Hint: make sure not to include the lines that start with 'From:'.
+#Also look at the last line of the sample output to see how to print the count.
+
+# fh = open('mbox-short.txt')
+fname = input('Enter file name:')
+
+try:
+    fh=open(fname)
+except:
+    print('File does not exist')
+    quit()
+
+count = 0
+for line in fh:
+    if line.startswith('From '):
+        words=line.split()
+        print(words[1])
+        count = count + 1
+print('There were', count,'lines in the file with From as the first word' )
