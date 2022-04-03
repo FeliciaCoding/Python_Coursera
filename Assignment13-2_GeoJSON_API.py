b02d01497df87031fa2a3502cfc10b7a1f96548d
FeliciaCoding <peiwenliao0703@gmail.com>
2022-04-03 16:53:36 +0200

    Using the GeoJSON API

---
+++ Assignment13-2_GeoJSON_API.py
@@ -0,0 +1,48 @@
+# Week 6 : Using the GeoJSON API
+
+import urllib.request, urllib.parse, urllib.error
+import json
+
+#url = 'http://py4e-data.dr-chuck.net/comments_42.json'
+#url= 'http://py4e-data.dr-chuck.net/comments_1414076.json'
+serviceurl = 'http://py4e-data.dr-chuck.net/json?'
+
+
+while True:
+    # South Federal University
+    address = input('Enter location: ')
+    if len(address) < 1:
+        print('No address...')
+        # Question: continue
+        quit()
+
+# Error: Address = peremeter + key: peremeter
+    url = serviceurl + urllib.parse.urlencode({'address': address, 'key' : 42})
+    print('Retrieving', url)
+
+
+    # send request and open the the url
+    #uh= urllib.request.urlopen(url)
+    uh = urllib.request.urlopen(url)
+
+
+    # read and decode to readable format
+    data=uh.read().decode()
+    print('Retriving', len(data), 'characters')
+
+
+    try:
+        js = json.loads(data)
+    except:
+        js = None
+
+    if not js or 'status' not in js or js['status'] != 'OK':
+        print('==== Failure To Retrieve ====')
+        print(data)
+        break
+
+    print(json.dumps(js, indent = 4))
+
+#Error: print('Count:', len(info)) # only counting list instead od dic
+    placeid = js["results"][0]['place_id']
+    print ("Place id",placeid)
