3cca6dbc720a1fa17e6db457b19367be96504929
FeliciaCoding <peiwenliao0703@gmail.com>
2022-05-27 11:56:05 +0200

    Adding

---
+++ geodata/where.js
@@ -0,0 +1,22 @@
+myData = [
+[46.2034937,6.1411093, 'Rue du Stand 60, 1204 Genève, Switzerland'],
+[50.06688579999999,19.9136192, 'aleja Adama Mickiewicza 30, 30-059 Kraków, Poland'],
+[52.2394019,21.0150792, 'Krakowskie Przedmieście 5, 00-068 Warszawa, Poland'],
+[30.0186205,31.5014854, 'AUC Avenue، Road، New Cairo 1, Cairo Governorate 11835, Egypt'],
+[34.0489281,-111.0937311, 'Arizona, USA'],
+[38.0399391,23.8030901, 'Monumental Plaza, Building C, 1st Floor, Leof. Kifisias 44, Marousi 151 25, Greece'],
+[28.3588163,75.58802039999999, 'Vidya Vihar, Pilani, Rajasthan 333031, India'],
+[6.891986300000001,3.7181286, '121103, Ilishan-Remo, Nigeria'],
+[25.2677203,82.99125819999999, 'Ajagara, Varanasi, Uttar Pradesh 221005, India'],
+[12.9504048,77.5020617, 'Mysore Rd, Jnana Bharathi, Bengaluru, Karnataka 560056, India'],
+[31.5488923,-97.1130573, '1311 S 5th St, Waco, TX 76706, USA'],
+[39.9619537,116.3662615, '19 Xin Wai Da Jie, Beitaipingzhuang, Hai Dian Qu, Bei Jing Shi, China, 100875'],
+[53.8930049,27.545623, 'prasp. Niezaliežnasci 4, Minsk, Belarus'],
+[44.8184518,20.4575913, 'Studentski trg 1, Beograd, Serbia'],
+[42.5030333,-89.0309048, '700 College St, Beloit, WI 53511, USA'],
+[53.8930049,27.545623, 'prasp. Niezaliežnasci 4, Minsk, Belarus'],
+[31.261426,34.7995546, 'David Ben Gurion Blvd 1, Beer Sheva, Israel'],
+[10.6779085,78.74454879999999, 'Palkalaiperur, Tiruchirappalli, Tamil Nadu 620024, India'],
+[42.3504997,-71.1053991, 'Boston, MA 02215, USA'],
+[35.3050053,-120.6624942, 'San Luis Obispo, CA 93407, USA']
+];
